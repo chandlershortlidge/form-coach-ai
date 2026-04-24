9a10f1037d2aa888a2bf2080ef0be2bb81e95f3c
Chandler Shortlidge <chandlershortlidge@gmail.com>
2026-04-24 12:41:16 +0200

    refactored eval functions

--- llm-evals/eval_functions.py
+++ llm-evals/eval_functions.py
@@ -1,49 +1,46 @@
 from scipy.stats import bootstrap
 import numpy as np
+import scipy
 
 
 # Find the 95% Confidence Interval, Standard Error, and Margin of Error for Correctness and Hallucination
 
-def correctness_score(df, df_name):
-    # 1. turn score into list for boostrap function
-    correctness_array = df["correctness"].values
-    #2. Use bootstrap function to dinf the Confidence Interval (default is 0.95) and standard error
-    bootstrapped_hallucination = bootstrap(data=(correctness_array,), statistic=np.mean, method='BCa', rng=42)
-    result = bootstrapped_hallucination
-    print(f"95% CI Correctness Low - {df_name}: {result.confidence_interval.low * 2}") # denormalized
-    print(f"95% CI Correctness High - {df_name}: {result.confidence_interval.high * 2}") # deormalized
-    standard_erorr_correctness_normalized = result.standard_error *2 # denormalized
-    print(f"Standard Error - Correctness - {df_name}: {standard_erorr_correctness_normalized:.4f}")
-    # 3. find the margin of error for Hallucination: MOE = (CI High - CI Low) / 2
-    correctness_MOE = (result.confidence_interval.high *2 - result.confidence_interval.low *2) / 2 
-    print(f"Correctness MOE - {df_name}: {correctness_MOE:.2f}")
+def compute_bootstrap(df, column):
+    """Compute the CI high, CI Low,
+    standard error and margin of error (MOE) for a given dataframe and column"""
+    clean_values = df[column].dropna() # drop NaNs
+    df_array = clean_values.values #values converts and pandas Series into a numpy array
+    result = bootstrap(
+        data=(df_array,),  # tuple containing array — what bootstrap wants
+        method='BCa',
+        statistic=np.mean,
+        rng=42
+    )
+    
+  
+    return result
 
+def print_bootstrap_results(result, df_name: str, column_name: str):
+    """With the copnute_bootstrap result, print the CI high, CI Low,
+    standard error and margin of error (MOE), denormalizing the results.""" 
+    print(f"--- {df_name}: {column_name} ---")
+    print(f"95% CI Low: {result.confidence_interval.low * 2}")
+    print(f"95% CI High: {result.confidence_interval.high * 2}")
+    print(f"Standard error: {result.standard_error * 2}")
+    MOE = (result.confidence_interval.high *2 - result.confidence_interval.low *2)
+    print(f"MOE: {MOE:.2f}")
+    
 
-def hallucination_score(df, df_name):
-    # 1. turn score into list for boostrap function
-    hallucination_array = df["hallucination"].values
-    #2. Use bootstrap function to dinf the Confidence Interval (default is 0.95) and standard error
-    bootstrapped_hallucination = bootstrap(data=(hallucination_array,), statistic=np.mean, method='BCa', rng=42)
-    result = bootstrapped_hallucination
-    print(f"95% CI Hallucination Low - {df_name}: {result.confidence_interval.low}")
-    print(f"95% CI Hallucination High - {df_name}: {result.confidence_interval.high}")
-    print(f"Standard Error - Hallucination - {df_name}: {result.standard_error:.4f}")
-    # 3. find the margin of error for Hallucination: MOE = (CI High - CI Low) / 2
-    hallucination_MOE = (result.confidence_interval.high - result.confidence_interval.low) / 2
-    print(f"hallucination_MOE - {df_name}: {hallucination_MOE:.2f}")
+def get_score(df, column: str):
+    """Print the mean score for the column in the dataframe"""
+    df_clean = df[column].dropna()
+    df_array = df_clean.values
+    score = df_array.mean() * 2
+    print(f"Score: {score}")
 
+def do_t_test(baseline_df, new_df, column):
+    baseline_score = baseline_df[column].dropna()
+    new_score = new_df[column].dropna()
 
-
-def hallucination_score_normalized(df, df_name):
-    # 1. turn score into list for boostrap function
-    hallucination_array = df["hallucination"].values
-    #2. Use bootstrap function to dinf the Confidence Interval (default is 0.95) and standard error
-    bootstrapped_hallucination = bootstrap(data=(hallucination_array,), statistic=np.mean, method='BCa', rng=42)
-    result = bootstrapped_hallucination
-    print(f"95% CI Hallucination Low - {df_name}: {result.confidence_interval.low * 2}")
-    print(f"95% CI Hallucination High - {df_name}: {result.confidence_interval.high * 2}")
-    standard_erorr_hallucination_normalized = result.standard_error *2 # denormalized
-    print(f"Standard Error - Hallucination - {df_name}: {standard_erorr_hallucination_normalized:.4f}") # denormalize
-    # 3. find the margin of error for Hallucination: MOE = (CI High - CI Low) / 2
-    hallucination_MOE = (result.confidence_interval.high *2 - result.confidence_interval.low *2) / 2
-    print(f"hallucination_MOE - {df_name}: {hallucination_MOE:.2f}")
+    result = scipy.stats.ttest_rel(baseline_score, new_score)
+    return result
